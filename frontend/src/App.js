1d029aa8b52b36aa9d259d13cc00916d74b2a9d4
shreyaa-0 <saishrey04@gmail.com>
2024-12-12 14:07:36 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+// src/App.js
+// import React from 'react';
+// import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+// import GPTChatbot from './Pages/GPTChatbot';
+
+// function App() {
+//     return (
+//         <Router>
+//             <Routes>
+//                 <Route path="/" element={<GPTChatbot />} />
+//             </Routes>
+//         </Router>
+//     );
+// }
+
+// export default App;
---
+++ frontend/src/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import Router from "./Router";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={Router} />
+  </React.StrictMode>
+);
+
+reportWebVitals();
+
+
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import { RouterProvider } from "react-router-dom"; 
+// import "./index.css";
+// import reportWebVitals from "./reportWebVitals";
+// import App from "./App";  // Import your main App component, which handles routing
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <App />  {/* Render your App component */}
+//   </React.StrictMode>
+// );
+
+// reportWebVitals();
+
